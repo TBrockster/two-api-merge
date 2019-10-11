febc0a0d6d0ea1376f903ca3120dafd763cb9e04
TBrockster <tobybrock93@gmail.com>
2019-10-11 12:03:48 +0100

    final commit

--- __tests__/standing_lookup_test.js
+++ __tests__/standing_lookup_test.js
@@ -1,4 +1,4 @@
-const {StandingsLookup, getCompIDs} = require('../standing_lookup')
+const {StandingsLookup, getCompIDs, standings} = require('../standing_lookup')
 
 test('return array of ids from competition lookup', async () => {
   expect(await StandingsLookup()).toEqual(['4oogyu6o156iphvdvphwpck10', '2kwbbcootiqqgmrzs6o5inle5'])
@@ -10,4 +10,8 @@ test('extracting ids from competitions object', () =>{
                         {"id": "2"}
                         ]
   expect(getCompIDs(competitions)).toEqual(["1", "2"])
+})
+
+test('see data', async () => {
+  expect(await standings(['4oogyu6o156iphvdvphwpck10', '2kwbbcootiqqgmrzs6o5inle5'])).not.toBe(undefined)
 })
--- standing_lookup.js
+++ standing_lookup.js
@@ -16,7 +16,21 @@ getCompIDs = (competitions) => {
   })
 }
 
+standings = (comp_ids) => {
+  const url = 'https://makers.sd.dazn-dev.com/api/competitions';
+  comp_ids.forEach(id => {
+    return axios.get(`${url}` + "/" + `${id}`)
+    .then(response => response.data)
+    .then((data) => {
+      return data
+    })
+  })
+}
+
+
+
 
 
 module.exports.StandingsLookup = StandingsLookup;
 module.exports.getCompIDs = getCompIDs;
+module.exports.standings = standings;
