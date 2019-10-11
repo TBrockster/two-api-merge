05c5143a3eedbfbf73381376ce8c7a66952b9056
TBrockster <tobybrock93@gmail.com>
2019-10-11 11:46:14 +0100

    refactor of previous commit

--- __tests__/standing_lookup_test.js
+++ __tests__/standing_lookup_test.js
@@ -1,15 +1,13 @@
 const {StandingsLookup, getCompIDs} = require('../standing_lookup')
 
-test('return json from lookup', async () => {
+test('return array of ids from competition lookup', async () => {
   expect(await StandingsLookup()).toEqual(['4oogyu6o156iphvdvphwpck10', '2kwbbcootiqqgmrzs6o5inle5'])
 })
 
 test('extracting ids from competitions object', () =>{
-  const competitions = [{
-    "id": "1"
-  },
-    {
-      "id": "2"
-    }]
+  const competitions = [
+                        {"id": "1"},
+                        {"id": "2"}
+                        ]
   expect(getCompIDs(competitions)).toEqual(["1", "2"])
 })
